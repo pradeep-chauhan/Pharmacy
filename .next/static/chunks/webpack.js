7e4108db41eb1ca9a6d3805f715eb7c3dd376205
Jain-Daksh <daksh44jain@gmail.com>
2022-09-13 14:17:57 +0530

    changed gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f32e0e342a026448"; }
+/******/ 		__webpack_require__.h = function() { return "2a9356a1970903db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
