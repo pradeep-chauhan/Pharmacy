20bc11d5110eca2f411bc5c3bc611d91333846fb
Jain-Daksh <daksh44jain@gmail.com>
2022-10-21 20:33:31 +0530

    info section spacing inc

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/future/image'
-import { Container } from 'react-bootstrap';
 import Svg1 from '../images/semicircle(1).png'
 import Svg2 from '../images/zigzag-lines-in-side-view-position.png'
 import { People, Award,Book , JournalMedical} from 'react-bootstrap-icons'
@@ -14,11 +13,9 @@ function Info() {
               <div className="mt-5">
                 <Image src={Svg1} className="homepagesvg3" />
                 <h1>Reason to choose Gurukul Pharmacy</h1>
-
                 <p className="paragraph">Along with lessons, answers, study resources, grade assignments, feedback and one-to-one
                   attention, The
                   experienced tutors at Learning Space also extend their support in writing.</p>
-
               </div>
               <button type="button" className="buttoninfo">Explore Now</button>
               <div className="d-none d-sm-block d-md-block  ">
@@ -31,10 +28,9 @@ function Info() {
                   <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                     <div className="container">
                       <div className="row">
-                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12  card1 card5 mb-2">
+                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12  card1 card5 mb-3">
                           <div className="card card5 card6">
                             <div className="card-body">
-
                               <People className='fas fa-graduation-cap icon-info mt-3 mb-3' />
                               <h4 className="card-heading">Experts Teacher</h4>
                               <p className="card-text">University of Gurukul Pharmacy is a leading modern university with a tradition of
@@ -42,7 +38,7 @@ function Info() {
                             </div>
                           </div>
                         </div>
-                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12 card2 card5 mb-2">
+                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12 card2 card5 mb-3">
                           <div className="card card5 card6">
                             <div className="card-body">
                               <Award className='fa fa-graduation-cap icon-info mt-3 mb-3' />
@@ -60,8 +56,7 @@ function Info() {
                   <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                     <div className="container">
                       <div className="row">
-                       
-                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12 card3 card5 mb-2">
+                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12 card3 card5 mb-3">
                           <div className="card card5 card6">
                             <div className="card-body">
                               <JournalMedical className='fas fa-graduation-cap   icon-info mt-3 mb-3' />
@@ -72,10 +67,10 @@ function Info() {
                             </div>
                           </div>
                         </div>
-                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12  card5 mb-2">
+                        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12  card5 mb-3">
                           <div className="card card5 card6">
                             <div className="card-body">
-                              <Book className='fas fa-graduation-cap   icon-info mt-3 mb-3' />
+                              <Book className='fas fa-graduation-cap icon-info mt-3 mb-3' />
                               <h4 className="card-heading">Library</h4>
                               <p className="card-text">The institute have a very good Library with many Reference, Text Books and Computer Facilities</p>
                             </div>
