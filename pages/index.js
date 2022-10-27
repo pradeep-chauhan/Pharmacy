5397b6dac4a1ed6972c552d9ef3d5025714751a4
Pradeep <pradeep@Pradeeps-MacBook-Pro.local>
2022-10-27 19:24:15 +0530

    fixed mata tag

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
 import Homepage from '../components/Homepage'
-import WhyUs from '../components/WhyUs'
+// import WhyUs from '../components/WhyUs'
 import Info from '../components/Info'
-import Offer from '../components/Offer'
-import Category from '../components/Courses'
-import Gallery from '../components/Gallery'
-import Testimonial from '../components/Testimonial'
+// import Offer from '../components/Offer'
+// import Category from '../components/Courses'
+// import Gallery from '../components/Gallery'
+// import Testimonial from '../components/Testimonial'
 import ContactUs from '../components/ContactUs'
 import Footer from '../components/Footer'
 import CareerPath from '../components/CareerPath'
 import Infrastructure from '../components/Infrastructure'
-import Faculties from '../components/Faculties'
+// import Faculties from '../components/Faculties'
 import Vision from '../components/Vision'
 import Committees from '../components/Committees'
 import Course from '../components/Course'
@@ -24,9 +24,9 @@ export default function Home() {
       <Head>
         <title>Gurukul Pharmacy | Pharmacy College</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="title" content="Gurukul Pharmacy | Pharmacy College" />
-        <meta name="description" content="Gurukul pharmacy college work right at the heart of human healthcare, taking on  roles relating to the design and development of new treatments, prescription and care management, and advising on the range of medical options as well and also provides lots of courses which helps grow higher in life and courses such as D-pharma.Pharm D or Doctor of Pharmacy is a professional degree in pharmacy stream. Gurukul Pharmacy Pharm D course duration is six years in total inclusive of 5 years of learning and one year of internship for practical learning. The only doctoral degree that can be pursued right after passing 10+2 qualifying examinations." key="desc" /> 
-        <meta name='keywords' content="Pharmacy college, Udaipur Pharmacy College, Best Pharmacy College, Apothecary,Pharmacopoeia, Best Pharmacy College in Udaipur, Best Pharmacy College in Rajasthan,Best Udaipur College,Udaipur best pharmacy college,Gurukul Pharmacy College" />
+        <meta property="og:title" content="Gurukul Pharmacy | Pharmacy College" key="title"/>
+        <meta property="og:description" content="Gurukul pharmacy college work right at the heart of human healthcare, taking on  roles relating to the design and development of new treatments, prescription and care management, and advising on the range of medical options as well and also provides lots of courses which helps grow higher in life and courses such as D-pharma.Pharm D or Doctor of Pharmacy is a professional degree in pharmacy stream. Gurukul Pharmacy Pharm D course duration is six years in total inclusive of 5 years of learning and one year of internship for practical learning. The only doctoral degree that can be pursued right after passing 10+2 qualifying examinations." key="desc" /> 
+        <meta property='og:keywords' content="Pharmacy college, Udaipur Pharmacy College, Best Pharmacy College, Apothecary,Pharmacopoeia, Best Pharmacy College in Udaipur, Best Pharmacy College in Rajasthan,Best Udaipur College,Udaipur best pharmacy college,Gurukul Pharmacy College" key="keywords"/>
 
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
