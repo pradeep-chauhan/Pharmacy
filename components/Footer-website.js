e96d6e5500e43fc373cc06a74c8393655fe62080
Jain-Daksh <daksh44jain@gmail.com>
2022-10-27 12:31:44 +0530

    footer section spacing remove

--- components/Footer-website.js
+++ components/Footer-website.js
@@ -9,11 +9,11 @@ function FooterWebsite() {
       <div className="container ">
       <hr></hr>
           <div className="row">
-            <div className="col-12 col-md-8 col-sm-12 col-xs-12 mt-3 mb-3 ">
+            <div className="col-12 col-md-8 col-sm-12 col-xs-12 mt-1 mb-1 ">
               © {year} Gurukul College of Pharmacy. All Rights Reserved
             </div>
 
-            <div className="col-12 col-md-4 col-sm-12 col-xs-12 mt-3 mb-3  ">
+            <div className="col-12 col-md-4 col-sm-12 col-xs-12 mt-1 mb-1  ">
             <h6>Design and Developed By <strong> <a href="https://alvrio.io/">Alvrio Labs</a></strong></h6>
             </div>
           </div>
