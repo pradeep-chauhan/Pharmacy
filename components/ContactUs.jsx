18ca97ccd08a73cf37db8a91bb9e3bdf41558efa
Jain-Daksh <daksh44jain@gmail.com>
2022-10-27 20:49:26 +0530

    contact us name changed

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -32,21 +32,7 @@ function ContactUs() {
       data: values
     };
     alert("We will get back to you shortly")
-  
-    
-
-
   }
-  //   function submitHandler() {
-  //     if (!field) {
-  //       setError(true);
-  //       return null;
-  //     }
-  //       return alert("We will get back to you shortly")
-  // }
-  // function submitHandler() {
-  //   return alert("We will get back to you shortly")
-  // }
   return (
     <div>
       <section className="form" id='contactus' onSubmit={handleSubmit(onSubmit)}>
@@ -54,12 +40,10 @@ function ContactUs() {
           <div className="row">
             <div itemProp='image' className="col-md-6 ">
               <Image src={Contact} className='image-contactus' />
-
             </div>
             <div className="col-md-6">
-              <h2 itemProp='name'>Contact Us</h2>
+              <h2 itemProp='name' className='heading'>Contact Us</h2>
               <p>To request a quote or want to meet up for coffee, contact us directly or fill out the form and we will get back to you promptly</p>
-
               <form className="form-info">
                 <div className="mb-3">
                   <label for="exampleInputName" className="form-label" name="name">Name</label>
@@ -74,18 +58,14 @@ function ContactUs() {
                     },
                     pattern: {
                       pattern: /^[A-Za-z]+$/
-
                     },
                     maxLength: {
                       value: 75,
                       message: "name cannot be more than 75"
                     },
-
                   })} className="form-control" id="exampleInputName" aria-describedby="NameHelp" required />
-
                   <span id="nameerror" className="text-danger font-weight-bold" ></span>
                   {errors.name && <p>Please check the name , name cannot be less than 2 </p>}
-
                 </div>
                 <div className="mb-3">
                   <label for="exampleInputEmail1" className="form-label" name="email" >Email address</label>
@@ -99,7 +79,6 @@ function ContactUs() {
                     },
                     pattern: {
                       pattern: /^[A-Za-z]+$/
-
                     },
                     maxLength: {
                       value: 75,
@@ -108,22 +87,18 @@ function ContactUs() {
                   })} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
                   <span id="emailerror" className="text-danger font-weight-bold" ></span>
                   {errors.email && <p>Please check the Email</p>}
-
                 </div>
                 <div className="mb-3">
                   <label for="message" className="form-label" name="message" >Message</label>
                   <textarea type="text"{...register("message")} className="form-control" id="exampleInputMessage" rows="4" required></textarea>
                 </div>
-                <button type="submit" onClick={(e) => { handleSubmit(e) }} className="buttoninfo  form-control">Send Message</button>
-                {/* <button type="submit" onClick={submitHandler} className="buttoninfo  form-control">Send Message</button> */}
+                <button type="submit" onClick={(e) => { handleSubmit(e) }} className="buttoninfo form-control">Send Message</button>
               </form>
             </div>
           </div>
-
         </div>
       </section>
     </div>
-
   )
 }
 export default ContactUs
