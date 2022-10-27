d8c07e3dd96cf5fd60c348a095fd412107430745
vivek-changwal <vivekchangwal@gmail.com>
2022-10-27 17:23:00 +0530

    schema name update

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,16 @@ export default function Home() {
   return (
     <div >
       <Head>
+      <meta name="Gurukul Pharmacy"/> 
+        <meta keywords="Pharmacy college, Udaipur Pharmacy College, Best Pharmacy College, Apothecary,Pharmacopoeia, Best Pharmacy College in Udaipur, Best Pharmacy College in Rajasthan,Best Udaipur College,Udaipur best pharmacy college,Gurukul Pharmacy College"/>
+        <meta content="Gurukul pharmacy college work right at the heart of human healthcare, taking on  roles relating to the design and development of new treatments, prescription and care management, and advising on the range of medical options as well and also provides lots of courses which helps grow higher in life and courses such as D-pharma.Pharm D or Doctor of Pharmacy is a professional degree in pharmacy stream. Gurukul Pharmacy Pharm D course duration is six years in total inclusive of 5 years of learning and one year of internship for practical learning. The only doctoral degree that can be pursued right after passing 10+2 qualifying examinations."/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="theme-color" key="Pharmacy college, Udaipur Pharmacy College, Best Pharmacy College, Apothecary,Pharmacopoeia, Best Pharmacy College in Udaipur, Best Pharmacy College in Rajasthan,Best Udaipur College,Udaipur best pharmacy college,Gurukul Pharmacy College "  content="Gurukul pharmacy college work right at the heart of human healthcare, taking on   roles relating to the design and development of new treatments, prescription and care management, and advising on the range of medical options as well and also provides lots of courses which helps grow higher in life and courses such as D-pharma.Pharm D or Doctor of Pharmacy is a professional degree in pharmacy stream. Gurukul Pharmacy Pharm D course duration is six years in total inclusive of 5 years of learning and one year of internship for practical learning. The only doctoral degree that can be pursued right after passing 10+2 qualifying examinations. "/>
-        <title>Gurukul Pharmacy</title>
+        <title>Gurukul Pharmacy/Pharmacy College</title>
       </Head>
       <Homepage />
       {/* <WhyUs /> */}
