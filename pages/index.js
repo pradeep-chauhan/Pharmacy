1e5321e6f9ad21c1f756d01a41ab67576611a5c3
vivek-changwal <vivekchangwal@gmail.com>
2022-10-27 17:55:13 +0530

    schema updated

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <title>Gurukul Pharmacy/Pharmacy College</title>
+        <title>Gurukul Pharmacy | Pharmacy College</title>
       </Head>
       <Homepage />
       {/* <WhyUs /> */}
